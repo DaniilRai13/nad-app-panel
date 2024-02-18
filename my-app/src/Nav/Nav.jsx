3c386352f3764441483d38a73318bb00d101c003
DaniilRai13 <testnet02@mail.ru>
2024-02-18 21:09:09 +0300

    feat: change structure for navigation

--- my-app/src/Nav/Nav.jsx
+++ my-app/src/Nav/Nav.jsx
@@ -5,12 +5,12 @@ import cls from './Nav.module.css'
 export const Nav = () => {
   return (
     <div className={cls.nav__container}>
-      <div className={cls.nav__container__link}>
-        <NavLink to='/uploadImg'>Загрузка</NavLink>
-      </div>
-      <div className={cls.nav__container__link}>
-        <NavLink to='/showWorks'>Работы</NavLink>
-      </div>
+      <NavLink className={`${cls.nav__container__link} btn`} to='/'>
+        <div >Загрузка</div>
+      </NavLink>
+      {/* <NavLink className={`${cls.nav__container__link} btn`} to='/showWorks'>
+        <div >Работы</div>
+      </NavLink> */}
     </div>
   )
 }
